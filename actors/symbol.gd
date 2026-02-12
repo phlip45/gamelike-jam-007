581e3c636751db7a0ca7545efb704f9b19213816
Matthew Gray <phlip45@gmail.com>
2026-02-11 23:54:53 -0800

    Symbol Fix - Background now correctly disappearing

--- actors/symbol.gd
+++ actors/symbol.gd
@@ -2,3 +2,9 @@ extends RichTextLabel
 class_name Symbol
 
 static var offset = Vector2(-10,-22)
+@onready var color_rect: ColorRect = $ColorRect
+
+func _ready() -> void:
+	visibility_changed.connect(line_of_sight_change)
+func line_of_sight_change():
+	color_rect.visible = visible
